bb148565190e9e7b24c55320327a3922a992fac8
David Morales <dmoralesx24@gmail.com>
2019-08-16 13:09:55 -0400

    little change

--- mailer/form_setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*Form settings*/
-	$subj = "New message from the site 'David's portfolio"; //letter subject
-	$to = 'dmoralesx24@gmail.com'; // Enter Your E-mail
-	$from = 'dmoralesx24@gmail.com'; // Admin e-mail
-	$fromName = 'Your Company Name'; // Your company name
-	$charset = 'UTF-8';
-?>
